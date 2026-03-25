b6cd7df129b89b4599096fb47dab67171a8230f4
Theo Schutt <schutt20@stanford.edu>
2026-03-24 17:17:41 -0700

    update run scripts

---
+++ run_plot_gd_only.sh
@@ -0,0 +1,23 @@
+# Configuration
+KMAX=3
+JMAX=11
+
+##### GIANT DONUTS #####
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T05:31:13.665_reason_20251119_zmode_1_7.asdf --state_key zmode_1_7 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T05:38:04.442_reason_20251119_zmode_1_8.asdf --state_key zmode_1_8 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T05:38:54.077_reason_20251119_zmode_2_5.asdf --state_key zmode_2_5 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T05:43:44.180_reason_20251119_zmode_3_5.asdf --state_key zmode_3_5 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T06:29:58.406_reason_20251119_jmode_1.asdf --state_key nullmode_1 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T06:30:29.595_reason_20251119_jmode_2.asdf --state_key nullmode_2 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T06:35:39.639_reason_20251119_jmode_3.asdf --state_key nullmode_3 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T06:41:20.477_reason_20251119_jmode_4.asdf --state_key nullmode_4 --kmax $KMAX --jmax $JMAX
+
+python fit_plot_dbl_zks_sens.py --gd_files /sdf/data/rubin/u/jmeyers3/projects/aos/Hartmann/dz_matrix/hartmann_zernike_sensitivity_group_2025-11-20T06:47:02.667_reason_20251119_jmode_5.asdf --state_key nullmode_5 --kmax $KMAX --jmax $JMAX
